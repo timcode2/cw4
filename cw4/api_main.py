e29b8c95c62b203693a58d830812e04c09e63e46
artemm <artem.m2013@gmail.com>
2023-07-16 21:10:14 +0300

    add method get_vacancies

--- cw4/api_main.py
+++ cw4/api_main.py
@@ -6,13 +6,27 @@ from abc import ABC, abstractmethod
 class VacancyAPI(ABC):
 
     @abstractmethod
-    def get_vacancy(self, keyword):
+    def get_vacancies(self, keyword):
         pass
 
 
 class HeadHunterAPI(VacancyAPI):
-    pass
+    def get_vacancies(self, keyword):
+        url = 'https://api.hh.ru/vacancies'
+        params = {'text': keyword, 'per_page': 100}
+        response = requests.get(url, params=params)
+        data = response.content.decode()
+        response.close()
+        return data
 
 
 class SuperJobAPI(VacancyAPI):
-    pass
+    def get_vacancies(self, keyword):
+        api_key: str = os.getenv('SUPERJOB_API_KEY')
+        params = {'keyword': keyword, 'per_page': 100, 'count': 100}
+        headers = {'X-Api-App-Id': api_key}
+        url = 'https://api.superjob.ru/2.0/vacancies/'
+        response = requests.get(url, params=params, headers=headers)
+        data = response.content.decode()
+        response.close()
+        return data
