1ff3292181e09d9c540cb60f2e3bfd9d928beb63
artemm <artem.m2013@gmail.com>
2023-07-16 21:13:32 +0300

    compleat initialization for class Vacancy add dunder method __str__ and __lt__

--- cw4/vacancy.py
+++ cw4/vacancy.py
@@ -1,2 +1,16 @@
+import json
+
+
 class Vacancy:
-    pass
+    def __init__(self, name, link, city, salary, requirement) -> None:
+        self.name = name  # название вакансии
+        self.link = link  # сылка на вакансию
+        self.city = city  # местоположение
+        self.salary = salary  # средняя заработная плата
+        self.requirement = requirement  # требования к вакансии
+
+    def __str__(self) -> str:
+        return json.dumps(self.__dict__, ensure_ascii=False, indent=2)
+
+    def __lt__(self, other):
+        return self.salary < other.salary
