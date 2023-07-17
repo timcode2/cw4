e9b9ecbff02e562e07c13f4a27ca998e8611c96e
artemm <artem.m2013@gmail.com>
2023-07-17 08:59:34 +0300

    some changes

--- cw4/json_main.py
+++ cw4/json_main.py
@@ -10,5 +10,5 @@ class VacancyStorage(ABC):
 
 class JSONVacancyStorage(VacancyStorage):
     def add_vacancy(self, vacancy_list):
-        with open('filtered_vacancy.json', 'w', encoding='utf-8') as f:
+        with open('vacancies.json', 'w', encoding='utf-8') as f:
             json.dump(vacancy_list, f, indent=4, ensure_ascii=False)
