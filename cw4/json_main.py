64549337bdc25dcc2d06571ccce673c33b9d46b9
artemm <artem.m2013@gmail.com>
2023-07-17 18:27:43 +0300

    add method get_vacancies_by_city

--- cw4/json_main.py
+++ cw4/json_main.py
@@ -7,8 +7,20 @@ class VacancyStorage(ABC):
     def add_vacancy(self, vacancy):
         pass
 
+    @abstractmethod
+    def get_vacancies_by_city(self, vacancies_list, city):
+        pass
+
 
 class JSONVacancyStorage(VacancyStorage):
     def add_vacancy(self, vacancy_list):
         with open('vacancies.json', 'w', encoding='utf-8') as f:
             json.dump(vacancy_list, f, indent=4, ensure_ascii=False)
+
+    def get_vacancies_by_city(self, vacancies_list, city):
+        city_vacancies = []
+        for vacancy in vacancies_list:
+            vacancy = json.loads(vacancy)
+            if vacancy["city"] == city:
+                city_vacancies.append(vacancy)
+        return city_vacancies
