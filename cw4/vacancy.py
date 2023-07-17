0c17458aa92c8dbdefb070b8c107cf3f0e3e6a5e
artemm <artem.m2013@gmail.com>
2023-07-17 18:32:32 +0300

    fix some attributes for class Vacancy

--- cw4/vacancy.py
+++ cw4/vacancy.py
@@ -2,12 +2,12 @@ import json
 
 
 class Vacancy:
-    def __init__(self, name, link, city, salary, requirement) -> None:
+    def __init__(self, name, url, city, salary, description) -> None:
         self.name = name  # название вакансии
-        self.link = link  # сылка на вакансию
+        self.url = url  # сылка на вакансию
         self.city = city  # местоположение
         self.salary = salary  # средняя заработная плата
-        self.requirement = requirement  # требования к вакансии
+        self.description = description  # требования к вакансии
 
     def __str__(self) -> str:
         return json.dumps(self.__dict__, ensure_ascii=False, indent=2)
